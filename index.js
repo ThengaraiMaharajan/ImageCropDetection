5db77ba04e8001ded0b4e1f4c9cbdf5e68d7f28b
DESKTOP-81HU2NM\TMS <tmaharajan1999@gmail.com>
2024-11-17 09:24:54 +0530

    completed basic operations

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const fragment = window.location.hash.substring(1);
-let MainUrlParams = new URLSearchParams(fragment);
-const token = MainUrlParams.get('token');
-// console.log(token);
-localStorage.setItem('token',token);
-
 const userDisclaimer = document.getElementById('user-disclaimer');
 const addImageDisclaimer = document.getElementById('add-images-disclaimer');
 const captureFileButton = document.getElementById('captureFile');
-const uploadFileButton = document.getElementById('uploadFile');
 const fileStatusTag = document.getElementById('fileStatus');
 let confirmCropBtn = document.getElementById('confirmCropButton');
 confirmCropBtn.style.display = 'none';
@@ -38,89 +31,8 @@ document.getElementById('submitImages').addEventListener('click', submitImagesFn
 document.getElementById('captureFileSubmitButton').addEventListener('click', uploadCaptureAPI);
 document.getElementById('rejectImage').addEventListener('click', rejectImageFn);
 
-function handleFileUpload(event) {
-  uploadFileButton.style.display = 'none';
-  captureFileButton.style.display = 'none';
-  const file = event.target.files[0];
-  if (file) {
-    if (file.type !== 'application/pdf') {
-      fileStatusTag.innerHTML = `<p class="text-danger">Error: Only PDF files are allowed.</p>`;
-      uploadFileButton.style.display = 'inline';
-      captureFileButton.style.display = 'inline';
-      return;
-    }
-    fileStatusTag.innerHTML = `
-    <p class="text-success">
-    <i class="fa fa-check"></i> ${file.name}
-    </p>
-    <button class="btn btn bg-main-theme btn-lg bigButton" onclick="submitFilesFn()">Submit</button>
-    `;
-    // console.log('File selected:', file);
-    uploadStatus = 'success';
-    uploadStatusCode = 1;
-    uploadedFileData = file;
-  } else {
-    uploadStatus = 'failure';
-    uploadStatusCode = 2;
-  }
-}
-
-async function submitFilesFn() {
-  addImageDisclaimer.style.display = 'none';
-  captureFileButton.style.display = 'none';
-  submitImagesBtn.style.display = 'none';
-  userDisclaimer.style.display = 'none';
-  fileStatusTag.style.display = 'none';
-  document.getElementById('thumbnails').style.display = 'none';
-  const uploadFileToAPI = document.getElementById('uploadFileToAPI');
-  uploadFileToAPI.innerHTML = `
-    <h4 class="text-center" >Uploading PDF for Motor Claim Form</h4>
-    <div class="d-flex justify-content-center" ><img src="./assets/pictures/fileUploadGif.gif" alt="loader Gif" /></div>
-  `
-  const uploadFileToAPIStatus = document.getElementById('uploadFileToAPIStatus');
-  uploadFileToAPIStatus.innerHTML = `
-    <p class="m-0 p-0" > Uploading your PDF</p>
-  `
-  try {
-    let formData = new FormData();
-    formData.append('file', uploadedFileData, uploadedFileData.name);
-    formData.append('fileType', 'pdf');
-    formData.append('page', 1);
-    formData.append('isFinalPage', true);
-    formData.append('uniqueRequestID',0);
-    formData.append('qrid', token);
-    
-    const response = await fetch('https://uat-aki-api-v2.claims.digital/api/v3/webapi/admin/web/OCRFileUpload', {
-      method: 'POST',
-      body: formData
-    });
-    
-    if (response.ok) {
-      responseFromApi = await response.json();
-      let resposeAttachmentId = responseFromApi.rObj.attachmentID;
-      
-      if (responseFromApi.rcode == 1) {
-        setTimeout(() => {
-        getApiData(resposeAttachmentId);
-        }, 5000);
-      }else  if (responseFromApi.rcode == 3) {
-        somethingWentWrong();
-      } else {
-        somethingWentWrong();
-      }
-    } else {
-      throw new Error('Network response was not ok.');
-    }
-  
-  } catch (error) {
-    console.error('Error uploading PDF:', error);
-    somethingWentWrong();
-  }
-}
-
 function displayImage(event) {
   userDisclaimer.style.display = 'none';
-  uploadFileButton.style.display = 'none';
   captureFileButton.style.display = 'none';
   thumbnails.style.display = 'none';
   submitImagesBtn.style.display = 'none';
@@ -143,9 +55,6 @@ function displayImage(event) {
     };
     reader.readAsDataURL(input.files[0]);
   }
-
-
-  
 }
 document.getElementById('imageInput').addEventListener('change', displayImage);
 
@@ -289,7 +198,6 @@ function updateGallery() {
     // Append container to thumbnails
     thumbnails.appendChild(thumbnailContainer);
   });
-  // console.log(images);
   if(images.length>=1){
     submitImagesBtn.style.display = 'inline';
   }else{
@@ -298,8 +206,8 @@ function updateGallery() {
 }
 
 function deleteImage(index) {
-  images.splice(index, 1); // Remove the image from the array
-  updateGallery(); // Update the gallery display
+  images.splice(index, 1);
+  updateGallery();
 }
 
 
@@ -321,69 +229,18 @@ async function submitImagesFn(){
   captureFileButton.style.display = 'none';
   submitImagesBtn.style.display = 'none';
   document.getElementById('thumbnails').style.display = 'none';
-  // console.log('image collection before conversion: ', images);
   const uploadFileToAPI = document.getElementById('uploadFileToAPI');
   uploadFileToAPI.innerHTML = `
-    <h4 class="text-center" >Uploading Images for Motor Claim Form</h4>
+    <h4 class="text-center" >Processing Images</h4>
     <div class="d-flex justify-content-center" ><img src="./assets/pictures/fileUploadGif.gif" alt="loader Gif" /></div>
   `
   const uploadFileToAPIStatus = document.getElementById('uploadFileToAPIStatus');
   const token = localStorage.getItem('token');
   for (let i = 0; i < images.length; i++) {
     uploadFileToAPIStatus.innerHTML = `
-      <p class="m-0 p-0"> <strong>Uploading ${i + 1} / ${images.length} images</strong> </p>
+      <p class="m-0 p-0"> <strong>Processing ${i + 1} / ${images.length} images</strong> </p>
     `;
-    try {
-      let formData = new FormData();
-      formData.append('file', images[i].blob, `image${i + 1}.png`);
-      formData.append('fileType', 'image');
-      formData.append('page', i + 1);
-      formData.append('isFinalPage', images.length === i + 1);
-      if(responseFromApi.rObj){
-        formData.append('uniqueRequestID',responseFromApi.rObj.attachmentID);
-      }else{
-        formData.append('uniqueRequestID',0);
-      }
-      formData.append('qrid', token);
-      responseFromApi = await sendByteArray(formData);
-      // console.log("responseFromApi", responseFromApi);
-      // console.log(`Successfully uploaded image ${i + 1}`);
-    } catch (error) {
-      console.error(`Error uploading image ${i + 1}:`, error);
-      somethingWentWrong();
-    }
   }
-  let resposeAttachmentId ;
-  if(responseFromApi.rObj){
-    resposeAttachmentId = responseFromApi.rObj.attachmentID
-  }
-  if(responseFromApi.rcode == 1){
-    
-    uploadFileToAPI.innerHTML = `
-    <h4 class="text-center" >Processing Images for Motor Claim Form</h4>
-    <div class="d-flex justify-content-center" ><img src="./assets/pictures/fileUploadGif.gif" alt="loader Gif" /></div>
-  `
-  uploadFileToAPIStatus.innerHTML = ``;
-getApiData(resposeAttachmentId);
-  }else  if (responseFromApi.rcode == 3) {
-    somethingWentWrong();
-  }else{
-    somethingWentWrong();
-  }
-  
-
-}
-
-async function sendByteArray(formData) {
-  const response = await fetch('https://uat-aki-api-v2.claims.digital/api/v3/webapi/admin/web/OCRFileUpload', {
-    method: 'POST',
-    body: formData
-  });
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  const responseObject = await response.json(); // Add await here
-  return responseObject;
 }
 
 
@@ -411,61 +268,4 @@ function rejectImageFn() {
     cropper = null;
   }
   updateGallery();
-  // submitImagesBtn.style.display = 'inline';
-}
-
-async function getApiData(attachmentID) {
-  const url = `https://uat-aki-api-v2.claims.digital/api/v3/webapi/admin/web/OCRParseResponse?AttachmentID=${attachmentID}&qrid=${token}`;
-
-  try {
-    const response = await fetch(url, {
-      method: 'GET'
-    });
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    const data = await response.json();
-    // console.log('Received data:', data);
-    if(data.rcode == 1){
-      uploadFileToAPI.innerHTML = ``
-      uploadFileToAPIStatus.innerHTML = `
-      <div class="d-flex justify-content-center align-items-center flex-dir-col" >
-        <div>
-          <p class="m-1 p-1">Reference ID : <strong># ${data.rObj.referenceNo}</strong> </p>
-        </div>
-        <div>
-          <h4 class="m-1 p-1 text-center"> <strong>Motor Claim Forms Uploaded Successfully</strong> </h4>
-          <p class="m-1 p-1 text-center">Please navigate back to the Bima Yangu Admin Portal to view the policy and claim alerts, as well as the completed form extraction results.</p>
-        </div>
-      </div>
-    `;
-    }else if(data.rcode == -1){
-      somethingWentWrong();
-    }
-    else{
-      somethingWentWrong();
-    }
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    somethingWentWrong();
-  }
-} 
-
-function openBimayanguApp(){
-  const otherProjectUrl = `https://uat-aki-v2.claims.digital/form-extract/form-extraction`;
-  // Redirect to the other project
-  window.location.href = otherProjectUrl;
-}
-
-function somethingWentWrong(){
-  uploadFileToAPI.innerHTML = `<p class="m-1 p-1 text-center"><strong>Request New QR Code for Scanning Another Motor Claim Form</strong></p>`;
-        uploadFileToAPIStatus.innerHTML = `
-          <div class="d-flex justify-content-center align-items-center flex-dir-col">
-            <div>
-              <p class="m-1 p-1 text-center">You have already used the QR code image to capture/upload motor claim forms. Please generate a new QR code in the admin panel for scanning another motor claim form.</p>
-            </div>
-          </div>
-        `;
 }
